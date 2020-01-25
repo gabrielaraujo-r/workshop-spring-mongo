f3dddcf5ac0f5b08352e007cc4f01f467986dcb0
Gabriel Araujo <matadoxx@gmail.com>
2020-01-25 03:19:21 -0300

    Referencing user posts

--- src/main/java/com/gabrielaraujo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gabrielaraujo/workshopmongo/config/Instantiation.java
@@ -22,6 +22,8 @@ public class Instantiation implements CommandLineRunner {
 	
 	@Autowired
 	private PostRepository postRepository;
+	
+	// private Faker faker = new Faker();
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -41,6 +43,9 @@ public class Instantiation implements CommandLineRunner {
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
